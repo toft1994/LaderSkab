fa634032be02a270e82f1197f3b90d43db5fc553
Thomas <thomas.baerentsen@gmail.com>
2020-03-11 16:14:38 +0100

    Finished ChargeControl unit tests

--- Laderskab.Test.Unit/ChargeControlUnitTests.cs
+++ Laderskab.Test.Unit/ChargeControlUnitTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Laderskab.Display;
 using NUnit.Framework;
 using UsbSimulator;
 using NSubstitute;
@@ -27,11 +28,79 @@ namespace Laderskab.Test.Unit
             _uut = new ChargeControl.ChargeControl(_display, _usbCharger);
         }
 
-        [TestCase()]
-        public void IsConnected_DifferentCases_ConnectionStatusAccurate()
+        [TestCase(true)]
+        [TestCase(false)]
+        public void IsConnected_DifferentCases_ConnectionStatusAccurate(bool status)
         {
-            _usbCharger.Connected.Returns(true);
-            Assert.That(_uut.IsConnected(), Is.True);
+            _usbCharger.Connected.Returns(status);
+            Assert.That(_uut.IsConnected(), Is.EqualTo(status));
+        }
+
+        [Test]
+        public void StartCharge_usbChargerSimulatorStartChargeIsCalled()
+        {
+            //Call the method
+            _uut.StartCharge();
+
+            //Assert that the method correctly calls StartCharge on the usbCharger
+            _usbCharger.Received().StartCharge();
+        }
+
+        [Test]
+        public void StopCharge_usbChargerSimulatorStopChargeIsCalledDisplaySetToNothing()
+        {
+            //Call the method
+            _uut.StopCharge();
+
+            //Assert that the method correctly calls StartCharge on the usbCharger
+            _usbCharger.Received().StopCharge();
+
+            Assert.That(_display.CurrentChargeId, Is.EqualTo(DisplayChargeId.Nothing));
+        }
+
+        [Test]
+        public void CurrentValueEvent_Over500_StopChargeShortCircuit()
+        {
+            //Raise the even from usbCharger
+            _usbCharger.CurrentValueEvent +=
+                Raise.EventWith(new CurrentEventArgs() {Current = 600});
+
+            //Since the handler calls StopCharge on itself, we can test
+            //If StopCharge has been called on usbCharger
+            _usbCharger.Received().StopCharge();
+
+            //Assert display is set to the right thing
+            Assert.That(_display.CurrentChargeId, Is.EqualTo(DisplayChargeId.ShortCircuit));
+            //Assert that updateDisplay has been called
+            _display.Received().UpdateDisplay();
+        }
+
+        [TestCase(500)]
+        [TestCase(5)]
+        [TestCase(250)]
+        public void CurrentValueEvent_Between500And5_CurrentChargeIdCharging(int current)
+        {
+            //Raise event
+            _usbCharger.CurrentValueEvent +=
+                Raise.EventWith(new CurrentEventArgs() {Current = current});
+
+            //Assert that _display.CurrentChargeId is Charging
+            Assert.That(_display.CurrentChargeId, Is.EqualTo(DisplayChargeId.Charging));
+        }
+
+        [Test]
+        public void CurrentValueEvent_Between0And5_CurrentChargeIdFullyChargedMethodsCalled()
+        {
+            _usbCharger.CurrentValueEvent +=
+                Raise.EventWith(new CurrentEventArgs() {Current = 2});
+
+            //Since the handler calls StopCharge on itself, we can test
+            //If StopCharge has been called on usbCharger
+            _usbCharger.Received().StopCharge();
+
+            Assert.That(_display.CurrentChargeId, Is.EqualTo(DisplayChargeId.FullyCharged));
+
+            _display.Received().UpdateDisplay();
         }
     }
 }
